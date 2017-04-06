7521b4cdb4fb503cb828d333d370bdaa8da9647d
BraulioRM <ramirez.braulioa@gmail.com>
2017-04-05 21:02:40 -0500

    Integration UI button  with java file

--- app/src/main/java/braulio/ramirez/numbershapes/MainActivity.java
+++ app/src/main/java/braulio/ramirez/numbershapes/MainActivity.java
@@ -2,12 +2,34 @@ package braulio.ramirez.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+
     }
+
+    public void testNumber (View view){
+
+        EditText userInput = (EditText)findViewById(R.id.UserInput);
+
+        String message= "";
+
+       // userInput.getText().toString();
+
+        Log.i("Numero ingresado", userInput.getText().toString());
+    }
+
 }
