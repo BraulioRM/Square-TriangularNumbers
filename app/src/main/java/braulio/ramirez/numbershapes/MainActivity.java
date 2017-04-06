1741e12bf3b3bcee041dd5baa3d16d7e01b98a00
BraulioRM <ramirez.braulioa@gmail.com>
2017-04-05 21:12:07 -0500

    Intelligence is integrated from coe made in Browxy

--- app/src/main/java/braulio/ramirez/numbershapes/MainActivity.java
+++ app/src/main/java/braulio/ramirez/numbershapes/MainActivity.java
@@ -29,7 +29,61 @@ public class MainActivity extends AppCompatActivity {
 
        // userInput.getText().toString();
 
-        Log.i("Numero ingresado", userInput.getText().toString());
+        //Log.i("Numero ingresado", userInput.getText().toString());
     }
+
+
+    class Number {
+
+        int number;
+
+        public boolean isSquare() {
+
+            double squareRoot = Math.sqrt(number);
+
+            if (squareRoot == Math.floor(squareRoot)) {
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        }
+
+        public boolean isTriangular() {
+
+            int x = 1;
+
+            int triangularNumber = 1;
+
+            while (triangularNumber < number) {
+
+                x++;
+
+                triangularNumber = triangularNumber + x;
+
+            }
+
+            if (triangularNumber == number) {
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        }
+
+
+    }
+
+
+
+
 
 }
